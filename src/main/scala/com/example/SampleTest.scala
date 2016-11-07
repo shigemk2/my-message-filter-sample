8b270298e37b07387e83d2f21923b6ada3cd980f
shigemk2 <i.am.shige@gmail.com>
2016-11-08 01:11:09 +0900

    Delete SampleTest

--- src/main/scala/com/example/SampleTest.scala
+++
@@ -1,6 +0,0 @@
-package com.example
-
-import akka.actor._
-
-object SampleTestDriver {
-}
--- src/test/scala/SampleTestSpec.scala
+++
@@ -1,7 +0,0 @@
-import org.scalatest._
-
-class MymessagefiltersampleSpec extends FlatSpec with Matchers {
-  "Hello" should "have tests" in {
-    true should === (true)
-  }
-}
